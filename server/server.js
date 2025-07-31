b91dc1ba9f07357bf38390aac61525c779c3448a
kaushal-kj <kaushalkumbhani17@gmail.com>
2025-07-31 14:23:25 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }));
 app.use(express.json());
 
 // Routes
